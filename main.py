65c03fdaeb74f5a9319881671d64781978afb426
Martin Meinero <m.meinero@alumno.um.edu.ar>
2026-09-23 20:03:44 -0300

    change name api.py

---
+++ main.py
@@ -0,0 +1,15 @@
+app = FastAPI()
+{
+    carreras = [
+    {
+        "Nombre": "Tecnicatura en desarrollo de software",
+        "facultad": "Ingenieria",
+        "Materias de primer año":[
+            {"name": "Matematica Discreta y Diseño Logico"},
+            {"name": "Analisis de Sistemas 1"},
+            {"name": "Álgebra y Geometria Analitica"},
+            {"name": "Ingles Técnico"},
+            {"name": "Informática"}]
+    }
+    ]
+}
